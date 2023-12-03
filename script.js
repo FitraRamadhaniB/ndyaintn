b71690b90a9e45186718354c504f1deab918575d
Fitra Ramadhani <115129261+FitraRamadhaniB@users.noreply.github.com>
2023-12-03 15:12:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Aaaaa, I Love You To, makasih ya udah maafin Rama ya SWEETYY. Maaf Rama belum bisa jadi apa yang sweetyy mau tapi Rama bakal berusaha untuk selalu ada buat sweetyy kapanpun itu dan dimanapun ituu. Lovee You Sweetyy";
+
+})
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+});
